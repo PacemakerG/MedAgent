9526aa5811aa38fade9c270b6114461cb573824e
yxh <1003306740@qq.com>
2026-03-09 10:33:59 +0000

    Support EPUB knowledge ingestion

--- backend/app/tools/__init__.py
+++ backend/app/tools/__init__.py
@@ -5,7 +5,13 @@ Exports all tool getter functions.
 
 from app.tools.llm_client import get_llm
 from app.tools.pdf_loader import (
+    load_document,
+    load_epub,
     load_pdf,
+    process_document,
+    process_document_with_metadata,
+    process_epub,
+    process_epub_with_metadata,
     process_knowledge_library,
     process_pdf,
     process_pdf_with_metadata,
@@ -24,8 +30,14 @@ __all__ = [
     "get_embeddings",
     "get_or_create_vectorstore",
     "get_retriever",
+    "load_document",
+    "load_epub",
     "load_pdf",
     "split_documents",
+    "process_document",
+    "process_document_with_metadata",
+    "process_epub",
+    "process_epub_with_metadata",
     "process_pdf",
     "process_pdf_with_metadata",
     "process_knowledge_library",
